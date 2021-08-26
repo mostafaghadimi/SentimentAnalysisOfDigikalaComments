533f99b02b88bd7b21d8a96790723c7c8a01f175
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2021-08-26 05:13:38 +0430

    :rainbow: logger method added

---
+++ digikala_comment_crawler/utils/logger.py
@@ -0,0 +1,15 @@
+import logging
+
+
+def get_global_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        '%(asctime)s:%(name)s:%(funcName)s:%(message)s',
+        datefmt='%Y/%m/%d %H:%M:%S'
+    )
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    return logger
